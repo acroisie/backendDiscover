6bcb829d7f67a97951087839e90cb8c0baf74f6e
Arnaud <acroisie@student.42lyon.fr>
2023-05-09 19:04:03 +0200

    WIP

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+	constructor(private usersService: UsersService) {}
 	@Post('/signup')
 	createUser(@Body() body: CreateUserDto){
-		console.log(body);	
+		this.usersService.createUser(body.email, body.password);
+	}
+
+	@Get('/:id')
+	findUser(@Param('id')id: string) {
+		return (this.usersService.findOne(parseInt(id)));
+	}
+
+	@Get()
+	findAllUsers(@Query('email')email: string) {
+		return (this.usersService.find(email));
+	}
+
+	@Delete('/:id')
+	removeUser(@Param('id')id: string) {
+		return (this.usersService.remove(parseInt(id)));
 	}
 }
