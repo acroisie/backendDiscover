fa06decf9eb864ea4c6e3bdc42ca7d42782bc7cf
Arnaud <acroisie@student.42lyon.fr>
2023-05-11 14:10:21 +0200

    implenment guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -7,6 +7,7 @@ import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -16,12 +17,8 @@ export class UsersController {
 		private authService: AuthService
 	) { }
 
-	// @Get('/whoami')
-	// whoAmI(@Session() session: any) {
-	// 	return (this.usersService.findOne(session.userId))
-	// }
-
 	@Get('/whoami')
+	@UseGuards(AuthGuard)
 	whoAmI(@CurrentUser() user: User) {
 		return (user);
 	}
