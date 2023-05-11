cfdc356dba96a011326630978613c24e8542f0eb
Arnaud <acroisie@student.42lyon.fr>
2023-05-11 13:59:34 +0200

    use global interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,6 +5,8 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -14,9 +16,14 @@ export class UsersController {
 		private authService: AuthService
 	) { }
 
+	// @Get('/whoami')
+	// whoAmI(@Session() session: any) {
+	// 	return (this.usersService.findOne(session.userId))
+	// }
+
 	@Get('/whoami')
-	whoAmI(@Session() session: any) {
-		return (this.usersService.findOne(session.userId))
+	whoAmI(@CurrentUser() user: User) {
+		return (user);
 	}
 
 	@Post('/signout')
