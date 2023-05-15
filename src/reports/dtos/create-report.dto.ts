6cfbb9eae53d0efa64716473c1e2e3a833d6262d
Arnaud <acroisie@student.42lyon.fr>
2023-05-15 09:40:12 +0200

    WIP

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,31 @@
+import { IsLatitude, IsLongitude, IsNumber, IsString, Max, Min } from "class-validator";
+
+export class CreateReportDto {
+	
+	@IsNumber()
+	@Min(0)
+	@Max(1000000000)
+	price: number;
+
+	@IsString()
+	make: string;
+
+	@IsString()
+	model: string;
+
+	@IsNumber()
+	@Min(1885)
+	@Max(2023)
+	year: number;
+
+	@IsLongitude()
+	lng: number;
+
+	@IsLatitude()
+	lat: number;
+
+	@IsNumber()
+	@Min(0)
+	@Max(10000000)
+	mileage: number;
+}
